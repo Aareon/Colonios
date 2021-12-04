6ae0dc44996c2e79ff9fa376905d5eb84c662346
Aareon Sullivan <askully13@gmail.com>
2021-12-04 16:33:01 -0600

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+# Start the game
 # included in pythonista
 from scene import *
 from ui import *
@@ -8,12 +9,12 @@ from random import randint, seed
 
 # project modules
 from lib import game
-from res.ui import Interface
+from interface import Interface
 
 HERE_FP = Path(__file__).parent
 
 if __name__ == "__main__":
-  iface = Interface()
-  game_inst = game.Game()
+    iface = Interface()
+    game_inst = game.Game()
   
-  iface.run(game_inst)
+    iface.run(game_inst)
