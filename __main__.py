2d8eb309ff01bc600fbd9ccb45949c80f5f789de
Aareon Sullivan <askully13@gmail.com>
2021-12-04 00:03:00 -0600

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,19 @@
+# included in pythonista
+from scene import *
+from ui import *
+
+# python std
+from pathlib import Path
+from random import randint, seed
+
+# project modules
+from lib import game
+from res.ui import Interface
+
+HERE_FP = Path(__file__).parent
+
+if __name__ == "__main__":
+  iface = Interface()
+  game_inst = game.Game()
+  
+  iface.run(game_inst)
